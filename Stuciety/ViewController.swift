4ad3de9b35a80946f94cb279a29f57afc86b99d6
Bryan Colin <bryancolin35@ymail.com>
2021-03-17 18:31:19 +0700

    Initial Commit

---
+++ Stuciety/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stuciety
+//
+//  Created by bryan colin on 3/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
