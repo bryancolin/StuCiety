b54564f75f9bbf4a755c3b335ee818f520f8fc41
Bryan Colin <53321614+bryancolin@users.noreply.github.com>
2021-09-22 14:19:27 +0700

    Update UI to iOS 15

--- StuCiety/Utilities/Custom UI/CustomUIInputBar.swift
+++ StuCiety/Utilities/Custom UI/CustomUIInputBar.swift
@@ -33,22 +33,19 @@ final class CustomUIInputBar: InputBarAccessoryView {
         inputTextView.textContainerInset = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
         inputTextView.placeholderLabelInsets = UIEdgeInsets(top: 8, left: 20, bottom: 8, right: 20)
         
-        if #available(iOS 13, *) {
-            inputTextView.layer.borderColor = UIColor(named: K.BrandColors.purple)?.cgColor
-        } else {
-            inputTextView.layer.borderColor = UIColor.lightGray.cgColor
-        }
+        inputTextView.layer.borderColor = UIColor(named: K.BrandColors.purple)?.cgColor
         
         inputTextView.backgroundColor = .clear
         inputTextView.layer.borderWidth = 1.0
         inputTextView.layer.cornerRadius = 7.0
+        inputTextView.tintColor = UIColor(named: K.BrandColors.purple)
         inputTextView.layer.masksToBounds = true
         inputTextView.scrollIndicatorInsets = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
     }
     
     func customSendButton() {
         sendButton.imageView?.backgroundColor = tintColor
-        sendButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 2, bottom: 2, right: 2)
+//        sendButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 2, bottom: 2, right: 2)
         sendButton.setSize(CGSize(width: 36, height: 36), animated: false)
         sendButton.image = #imageLiteral(resourceName: "send")
         sendButton.title = nil
